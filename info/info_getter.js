4de2316dd75ebd185f3c45ca4a7a8a5cf57ec9ac
SashaKYotoz <63205134+SashaKYotoz@users.noreply.github.com>
2026-02-03 23:31:34 +0200

    1 extra button

--- info/info_getter.js
+++ info/info_getter.js
@@ -4,7 +4,7 @@
 // async function getIpConfigInfo() {
 //     try {
 //         const response = await axios.get(PD_URL);
-        
+
 //         if (response.data && response.data.ip_data) {
 //             outputElement.innerText = response.data.ip_data;
 //         } else {
@@ -32,7 +32,7 @@ async function loadPanels() {
         const response = await fetch(GIST_URL);
         const data = await response.json();
 
-        grid.innerHTML = ''; 
+        grid.innerHTML = '';
         data.forEach(item => {
             const card = document.createElement('div');
             card.className = 'card';
@@ -40,23 +40,29 @@ async function loadPanels() {
                 <span class="icon">${item.icon}</span>
                 <span class="title">${item.title}</span>
             `;
-            
+
             card.onclick = () => {
                 modalTitle.innerText = item.title;
-                const shortcutText = Array.isArray(item.shortcuts) 
-        ? item.shortcuts.join('\n') 
-        : item.shortcuts;
+                const shortcutText = Array.isArray(item.shortcuts)
+                    ? item.shortcuts.join('\n')
+                    : item.shortcuts;
 
-    modalBody.innerText = shortcutText;
+                modalBody.innerText = shortcutText;
                 modal.style.display = "block";
             };
-            
+
             grid.appendChild(card);
         });
     } catch (error) {
         console.error("Error loading commands:", error);
     }
 }
+
+document.getElementById("back_to_main").onclick = () => document.location.replace(
+    document.location.href.includes("github")
+        ? "https://sashakyotoz.github.io/sashakyotoz_dev/"
+        : document.location.href.replace("info/info.html", "index.html")
+);
 
 closeBtn.onclick = () => modal.style.display = "none";
 window.onclick = (e) => { if (e.target == modal) modal.style.display = "none"; }
